87dc46dada3b9e5e32e0afa98f9a32898e12abec
Mayur Nandane <mayur.nandane@openconnect.com>
2018-08-17 19:05:55 +0530

    implemented hidden vertical scrolling

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import React, { Component } from 'react';
 import { CardList } from './cardlist/CardList';
 import { Searchbox } from './searchbox/Searchbox';
-import {robots} from './robots';
+import { Scroll } from './scroll/Scroll';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchString: ''
     }
   }
+
+  // to hold reference of this we will use ES6 syntax here for function
   onSearchKeyChange = (event) => {
     this.setState({
       searchString: event.target.value
     });
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => { return response.json() }).then(users => this.setState({ robots: users }));
+  }
+
   render() {
     const filteredRobots = this.state.robots.filter((robot) => {
       return robot.name.toLowerCase().includes(this.state.searchString);
     })
-
-    return (
-      <div className={'tc gradient'}>
-        <h1 className={'f1'}>Robots</h1>
-        <Searchbox searchChange={this.onSearchKeyChange}/>
-        <CardList robots={filteredRobots}/>
-      </div>
-    );
+    if (this.state.robots.length < 1) {
+      return <h1>Loading..</h1>
+    }
+    else {
+      return (
+        <div className={'tc gradient'}>
+          <h1 className={'f1'}>Robots</h1>
+          <Searchbox searchChange={this.onSearchKeyChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots}/>
+          </Scroll>
+        </div>
+      );
+    }
   }
 };
 
