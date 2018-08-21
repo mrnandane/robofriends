f320e3bd710115be486f5adb49eb2d84bba2b769
Mayur Nandane <mayur.nandane@openconnect.com>
2018-08-21 19:37:37 +0530

    added logger and implemented middleware

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './containers/App';
 import 'tachyons';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import registerServiceWorker from './registerServiceWorker';
 import {searchRobo} from './reducers';
 
-const store = createStore(searchRobo);
+/*NOTE: to apply middleware we need to import it from redux and call it as second parameter of createStore
+* with some callback function..we are using logger here..to create logs of action
+* */
+const logger = createLogger();
+const store = createStore(searchRobo, applyMiddleware(logger));
+
 
 ReactDOM.render(
   <Provider store={store}>
